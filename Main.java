9b8e2f0df5c8fc6f37fd355045d4c5504e20c291
Alessandro <sandroscro@libero.it>
2023-10-11 17:01:01 +0200

    Added Main

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int x=7, y=2;
+        x+=3;
+        y+=12;
+        System.out.println("The moltiplication of "+x+" and "+y+" is " +x*y);
+    }
+}
